c993558331bc0aa87f0865abbc7c6aa25728ec11
GabrielSDC <gabrielsantosdacosta2@gmail.com>
2025-11-23 17:47:57 -0300

    Adicionar imagem no fundo da tela

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -10,6 +10,8 @@ use Filament\Pages\Dashboard;
 use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
+use Filament\Support\Facades\FilamentView;
+use Filament\View\PanelsRenderHook;
 use Filament\Widgets\AccountWidget;
 use Filament\Widgets\FilamentInfoWidget;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
@@ -17,6 +19,7 @@ use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
 use App\Filament\Pages\LoginPage;
 
@@ -26,11 +29,12 @@ class AdminPanelProvider extends PanelProvider
     {
         return $panel
             ->default()
+            ->darkMode(false)
             ->id('admin')
             ->path('admin')
             ->login(LoginPage::class)
             ->colors([
-                'primary' => Color::Amber,
+                'primary' => '#3b250a',
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
--- resources/views/filament/pages/login.blade.php
+++ resources/views/filament/pages/login.blade.php
@@ -1,3 +1,18 @@
 <x-filament-panels::page.simple>
+    <style>
+        body {
+            background-image: url('{{ asset('images/login-background.svg') }}');
+            background-size: cover;
+            background-position: top;
+            background-color: white;
+        }
+
+        .fi-simple-main {
+            background: rgba(255, 255, 255, 0.75);
+            backdrop-filter: blur(10px);
+            color: black;
+        }
+    </style>
+
     {{ $this->content }}
 </x-filament-panels::page.simple>
