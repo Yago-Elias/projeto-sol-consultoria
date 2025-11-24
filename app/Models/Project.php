f9fa9460958117f7b8540dda1e526ad891ad761e
Rener Pontes <renercpontes@gmail.com>
2025-11-24 12:52:05 -0300

    Criado as models mais necessárias neste momento pra o desenvolvimento

---
+++ app/Models/Project.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Project extends Model
+{
+    protected $fillable = [
+        'name',
+        'description',
+        'image',
+        'company_name',
+        'company_email',
+        'project_price',
+        'estimated_cost',
+        'start_date',
+        'end_date',
+        'manager_id',
+    ];
+
+    public function manager(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function collaborators(): BelongsToMany
+    {
+        return $this->belongsToMany(
+            User::class,
+            'projects_users',
+            'user_id'
+        );
+    }
+
+    public function boards(): HasMany
+    {
+        return $this->hasMany(Board::class);
+    }
+}
