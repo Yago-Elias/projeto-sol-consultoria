446f92c6fdbb625151b075215292267c84ab9359
GabrielSDC <gabrielsantosdacosta2@gmail.com>
2025-12-15 21:35:00 -0300

    Atualização na estilização da barra de progresso

--- resources/views/filament/tables/columns/progress-bar.blade.php
+++ resources/views/filament/tables/columns/progress-bar.blade.php
@@ -1,6 +1,6 @@
 <div class="w-full flex flex-row gap-2 px-4">
-    <div class='w-full h-3/4 bg-(--neutro-2) rounded-md'>
-        <div class='h-full bg-(--cor-base) rounded-md'
+    <div class='w-full h-3/4 bg-(--color-gray-300) rounded-lg my-auto p-0.5'>
+        <div class='h-full bg-(--cor-base)/90 rounded-lg my-auto'
              style="width: {{ $record['Progresso'] }}%"></div>
     </div>
     <span><strong>{{ $record['Progresso'] }}%</strong></span>
