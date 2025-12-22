ff5bed65247616c1f801fa581c17646269a2a3e2
GabrielSDC <gabrielsantosdacosta2@gmail.com>
2025-12-22 17:28:44 -0300

    Criação da resource de usuário

---
+++ app/Filament/Resources/Users/Schemas/UserForm.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Filament\Resources\Users\Schemas;
+
+use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Schema;
+
+class UserForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')
+                    ->required(),
+                TextInput::make('email')
+                    ->label('Email address')
+                    ->email()
+                    ->required(),
+                DateTimePicker::make('email_verified_at'),
+                TextInput::make('password')
+                    ->password()
+                    ->required(),
+                TextInput::make('salary')
+                    ->required()
+                    ->numeric(),
+                FileUpload::make('image')
+                    ->image(),
+                TextInput::make('telephone')
+                    ->tel()
+                    ->required(),
+                Select::make('profile_id')
+                    ->relationship('profile', 'id'),
+                Select::make('role_id')
+                    ->relationship('role', 'id'),
+            ]);
+    }
+}
