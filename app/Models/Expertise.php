cf1a348eee5c4803b050d78eaa37990261a3891d
GabrielSDC <gabrielsantosdacosta2@gmail.com>
2025-12-23 18:39:26 -0300

    Criação do model de expertise e definição do relacionamento com user

---
+++ app/Models/Expertise.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Expertise extends Model
+{
+    protected $fillable = [
+        'expertise'
+    ];
+
+   public function users(): BelongsToMany
+   {
+       return $this->belongsToMany(User::class, 'users_expertises');
+   }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -82,4 +82,9 @@ class User extends Authenticatable
     {
         return $this->belongsTo(Role::class);
     }
+
+    public function expertises(): BelongsToMany
+    {
+        return $this->belongsToMany(Expertise::class, 'users_expertises');
+    }
 }
