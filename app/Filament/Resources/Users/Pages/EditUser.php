350f2010be8fdf0e203a968193d423498499c9a4
GabrielSDC <gabrielsantosdacosta2@gmail.com>
2026-01-04 18:46:35 -0300

    fix: url de imagens não se perdem mais na edição de consultor

--- app/Filament/Resources/Users/Pages/EditUser.php
+++ app/Filament/Resources/Users/Pages/EditUser.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\Users\Pages;
 
 use App\Filament\Resources\Users\UserResource;
+use App\Models\User;
 use Filament\Actions\DeleteAction;
 use Filament\Actions\ViewAction;
 use Filament\Resources\Pages\EditRecord;
@@ -17,5 +18,12 @@ class EditUser extends EditRecord
             ViewAction::make(),
             DeleteAction::make(),
         ];
+    }
+
+    protected function mutateFormDataBeforeSave(array $data): array
+    {
+        $data['image'] ??= $this->getRecord()['image'];
+
+        return $data;
     }
 }
