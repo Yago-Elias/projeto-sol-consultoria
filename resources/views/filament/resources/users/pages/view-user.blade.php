b2ee5798e57c38ed682aaced54ef5605b5aa468e
GabrielSDC <gabrielsantosdacosta2@gmail.com>
2026-01-06 14:10:51 -0300

    responsividade da tela de visualização de consultor

--- resources/views/filament/resources/users/pages/view-user.blade.php
+++ resources/views/filament/resources/users/pages/view-user.blade.php
@@ -1,12 +1,16 @@
+@php
+    $money = NumberFormatter::create('pt_BR', NumberFormatter::CURRENCY);
+@endphp
+
 <div>
     <x-filament::section>
         <x-slot name="heading">
             Informações
         </x-slot>
 
-        <div class="flex flex-row gap-8">
-            <div class="min-w-40 my-auto flex flex-col gap-y-2">
-                <x-filament::avatar class="w-40 h-40"
+        <div class="grid md:grid-cols-4 gap-8">
+            <div class="md:col-span-1 my-auto flex flex-col gap-y-2">
+                <x-filament::avatar class="w-40 h-40 mx-auto"
                     :src="filament()->getUserAvatarUrl($user)"
                     :alt="'imagem de '.$user['name']"
                     :attributes="
@@ -16,22 +20,30 @@
                 />
                 <p class="w-full text-center font-bold">{{ $user['name'] }}</p>
             </div>
-            <aside class="grid grid-cols-2 w-full gap-4">
+            <aside class="grid md:grid-cols-2 w-full gap-4 md:col-span-3">
                 <div class="flex flex-col gap-1">
                     <strong>Cargo</strong>
-                    <p class="p-2 rounded-lg border border-(--neutro-1)">{{ $user['role']['role'] }}</p>
+                    <x-filament::input.wrapper>
+                        <p class="p-2">{{ $user['role']['role'] }}</p>
+                    </x-filament::input.wrapper>
                 </div>
                 <div class="flex flex-col gap-1">
                     <strong>Salário</strong>
-                    <p class="p-2 rounded-lg border border-(--neutro-1)">{{ $user['salary'] }}</p>
+                    <x-filament::input.wrapper>
+                        <p class="p-2">{{ $money->format($user['salary']) }}</p>
+                    </x-filament::input.wrapper>
                 </div>
                 <div class="flex flex-col gap-1">
                     <strong>Telefone</strong>
-                    <p class="p-2 rounded-lg border border-(--neutro-1)">{{ $user['telephone'] }}</p>
+                    <x-filament::input.wrapper>
+                        <p class="p-2">{{ $user['telephone'] }}</p>
+                    </x-filament::input.wrapper>
                 </div>
                 <div class="flex flex-col gap-1">
                     <strong>Email</strong>
-                    <p class="p-2 rounded-lg border border-(--neutro-1)">{{ $user['email'] }}</p>
+                    <x-filament::input.wrapper>
+                        <p class="p-2">{{ $user['email'] }}</p>
+                    </x-filament::input.wrapper>
                 </div>
                 <div class="flex flex-col gap-1 col-span-full">
                     <strong>Áreas de Expertise</strong>
