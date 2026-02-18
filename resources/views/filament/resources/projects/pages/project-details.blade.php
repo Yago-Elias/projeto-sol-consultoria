dd82186b1e6220cc61dc23a05a9363768e39ecef
Rener Pontes <renercpontes@gmail.com>
2026-02-18 02:46:32 -0300

    Utilizando o método de usuário ao invés do atributo para recuperar a imagem

--- resources/views/filament/resources/projects/pages/project-details.blade.php
+++ resources/views/filament/resources/projects/pages/project-details.blade.php
@@ -53,8 +53,8 @@
                 <div class="flex items-center space-x-3">
                     <div class="flex-shrink-0">
                         <div class="w-12 h-12 rounded-full gb-gray-200 overflow-hidden flex items-center justify-center">
-                            @if($user->image)
-                                <img class="w-full h-full object-cover" src="{{ $user->image }}" alt="{{ $user->name }}">
+                            @if($user->getFilamentAvatarUrl())
+                                <img class="w-full h-full object-cover" src="{{ $user->getFilamentAvatarUrl() }}" alt="{{ $user->name }}">
                             @else
                                 <svg class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20">
                                     <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd" />
